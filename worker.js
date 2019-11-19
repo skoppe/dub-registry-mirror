be1273ea030a13eb8d3720227c4ef9251acf48e3
Sebastiaan Koppe <mail@skoppe.eu>
2019-11-19 15:55:25 +0100

    fix indentation and add some worker KV caching

--- worker.js
+++ worker.js
@@ -1,31 +1,50 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
 
+async function fetchWithCache(url) {
+    let result = await KV_DUB_REGISTRY.get(url)
+    if (result !== null)
+        return JSON.parse(result);
+
+    const response = await fetch(url);
+    if (!response.ok)
+        return response;
+
+    const data = await response.json();
+    await KV_DUB_REGISTRY.put(url, JSON.stringify(data));
+    return data;
+}
+
 async function handleRequest(request) {
-  const url = new URL(request.url);
-  if (request.url.indexOf("/api/") !== -1) {
-    const redirectUrl = "https://code.dlang.org" + url.pathname + url.search;
-    return Response.redirect(redirectUrl, 301);
-  }
+    try {
+        const url = new URL(request.url);
+        if (request.url.indexOf("/api/") !== -1) {
+            const redirectUrl = "https://code.dlang.org" + url.pathname + url.search;
+            return Response.redirect(redirectUrl, 302);
+        }
 
-  const parts = url.pathname.split('/');
-  if (parts[1] === 'packages') {
-    const packageName = parts[2];
-    const version = parts[3].slice(0,-4);
+        const parts = url.pathname.split('/');
+        if (parts[1] === 'packages') {
+            const packageName = parts[2];
+            const version = parts[3].slice(0,-4);
+            const infoUrl = `https://code.dlang.org/api/packages/${packageName}/info`
 
-    const result = await fetch(`https://code.dlang.org/api/packages/${packageName}/info`);
-    const package = await result.json();
-    const repo = package.repository;
-    const repoType = repo.kind;
-    switch (repoType) {
-      case 'github':
-        return Response.redirect(`https://github.com/${repo.owner}/${repo.project}/archive/v${version}.zip`, 301)
-      case 'gitlab':
-        return Response.redirect(`https://gitlab.com/${repo.owner}/${repo.project}/-/archive/v${version}/${repo.project}-v${version}.zip`, 301)
-      default:
-        return new Response(`Repository ${repoType} is not supported`,{status: 400})
+            let package = fetchWithCache(infoUrl);
+            const repo = package.repository;
+            const repoType = repo.kind;
+
+            switch (repoType) {
+            case 'github':
+                return Response.redirect(`https://github.com/${repo.owner}/${repo.project}/archive/v${version}.zip`, 301)
+            case 'gitlab':
+                return Response.redirect(`https://gitlab.com/${repo.owner}/${repo.project}/-/archive/v${version}/${repo.project}-v${version}.zip`, 301)
+            default:
+                return new Response(`Repository ${repoType} is not supported`,{status: 400})
+            }
+        }
+    } catch (e) {
+        return new Response("Error: " + e.message, {status: 400})
     }
-  }
-  return new Response("Unknown request", {status: 400})
+    return new Response("Unknown request", {status: 500})
 }
